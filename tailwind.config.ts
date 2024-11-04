12ca8ad15c39f8d6e37f63b33e66fbfa12566b30
richard stuart <richardstuart007@hotmail.com>
2024-11-04 21:56:55 +1300

    eslingrc and tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', './src/**/*.{css}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', './src/**/*.css'],
   theme: {
     extend: {
       gridTemplateColumns: {
