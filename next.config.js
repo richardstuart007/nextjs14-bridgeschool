cd1415fb2d2325711a1c1e716b3ccee388c42d04
richard stuart <richardstuart007@hotmail.com>
2024-12-28 13:33:15 +1300

    redirect to https://nextjs15-bridgeschool.vercel.app/

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*', // Redirect all paths
+        destination: 'https://nextjs15-bridgeschool.vercel.app/',
+        permanent: true
+      }
+    ]
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
